ab4994fcba4bd8e7629da60a6ec7d13f26e13f8d
Lee Jiwon <dlwldnjs427@naver.com>
2023-02-09 02:04:31 +0900

    first commit

---
+++ b1978.java
@@ -0,0 +1,26 @@
+package Math;
+
+import java.util.Scanner;
+
+public class b1978 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int sum = 0;
+		for(int i=0; i<N; i++) {
+			int S = sc.nextInt();
+			int count=0;
+			if(S==1)
+				continue;
+			for(int j=2; j<S; j++) {
+				if(S%j==0)
+					count++;
+			}
+			if(count==0)
+				sum++;
+		}
+		System.out.println(sum);
+	}
+
+}
