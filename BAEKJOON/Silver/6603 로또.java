66533ee386e0c89cdc9b894366cfeee9bdaa137b
jiwon <122508408+jw427@users.noreply.github.com>
2023-03-21 17:38:50 +0900

    Update and rename b6603.java to 6603 로또.java

--- BAEKJOON/Silver/6603 로또.java
+++ BAEKJOON/Silver/6603 로또.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class b6603 {
+public class Main {
 	static int k;
 	static StringBuilder sb = new StringBuilder();
 	static int[] S;
