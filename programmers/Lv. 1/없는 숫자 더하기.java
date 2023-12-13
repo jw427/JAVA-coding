6258005f2f23a43e0e0dfcb94413d5580db3b09f
jiwon <122508408+jw427@users.noreply.github.com>
2023-12-13 22:25:44 +0900

    :memo: 없는 숫자 더하기

---
+++ programmers/Lv. 1/없는 숫자 더하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0; // 출력할 결과
+        int[] arr = new int[10];
+        
+        for(int i=0; i<numbers.length; i++) {
+            arr[numbers[i]] = 1;
+        }
+        
+        for(int i=0; i<10; i++) {
+            if(arr[i] == 0) answer += i;
+        }
+        
+        return answer;
+    }
+}
